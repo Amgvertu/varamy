7ea94c6d7064df63e1d31ecf8f618d4479a86611
amgvertu <amgvertu@gmail.com>
2026-03-08 21:53:55 +0700

    Start

---
+++ src/main/java/info/prorabka/varamy/VaraMyApplication.java
@@ -0,0 +1,13 @@
+package info.prorabka.varamy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaraMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaraMyApplication.class, args);
+	}
+
+}
